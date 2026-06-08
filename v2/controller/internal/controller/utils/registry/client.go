3f6fcd30dd516829eba12ab493a548efe6bf364e
Alex Lee <3076438032@qq.com>
2026-06-08 16:51:35 +0800

    fix(controller): support bearer registry release retag (#32)

--- v2/controller/internal/controller/utils/registry/client.go
+++ v2/controller/internal/controller/utils/registry/client.go
@@ -15,13 +15,15 @@
 package registry
 
 import (
-	"bytes"
+	"crypto/tls"
 	"errors"
 	"fmt"
-	"io"
 	"net/http"
 
+	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
+	"github.com/google/go-containerregistry/pkg/v1/remote/transport"
 )
 
 // todo: refactor this struct, add opts for tls or something else
@@ -35,94 +37,75 @@ type BasicAuth struct {
 type Registry struct {
 	Host      string
 	BasicAuth BasicAuth
+	Insecure  bool
 }
 
 var ErrManifestNotFound = errors.New("manifest not found")
 
 // ReTag creates a new tag for an existing image by copying its manifest.
 func (c *Registry) ReTag(source, target string) error {
-	manifest, err := c.pullManifest(source)
+	sourceRef, err := name.ParseReference(source, c.nameOptions()...)
 	if err != nil {
+		return fmt.Errorf("failed to parse source image: %w", err)
+	}
+	targetRef, err := name.NewTag(target, c.nameOptions()...)
+	if err != nil {
+		return fmt.Errorf("failed to parse target image: %w", err)
+	}
+
+	descriptor, err := remote.Get(sourceRef, c.remoteOptions()...)
+	if err != nil {
+		if isManifestNotFound(err) {
+			return ErrManifestNotFound
+		}
 		return fmt.Errorf("failed to pull manifest for %s: %w", source, err)
 	}
-	if err := c.pushManifest(target, manifest); err != nil {
+	if err := remote.Tag(targetRef, descriptor, c.remoteOptions()...); err != nil {
 		return fmt.Errorf("failed to push manifest for %s: %w", target, err)
 	}
 	return nil
 }
 
-// todo: refactor this function, add opts for tls
-func (c *Registry) pullManifest(image string) ([]byte, error) {
-	ref, err := name.ParseReference(image)
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse image: %w", err)
-	}
-
-	url := fmt.Sprintf(
-		"http://%s/v2/%s/manifests/%s",
-		ref.Context().RegistryStr(),
-		ref.Context().RepositoryStr(),
-		ref.Identifier(),
-	)
-
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create GET request: %w", err)
-	}
-
-	req.SetBasicAuth(c.BasicAuth.Username, c.BasicAuth.Password)
-	req.Header.Set("Accept", "application/vnd.docker.distribution.manifest.v2+json")
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("failed to execute request: %w", err)
-	}
-	defer resp.Body.Close()
-
-	switch resp.StatusCode {
-	case http.StatusNotFound:
-		return nil, ErrManifestNotFound
-	case http.StatusOK:
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			return nil, fmt.Errorf("failed to read response body: %w", err)
+func (c *Registry) remoteOptions() []remote.Option {
+	auth := authn.Anonymous
+	if c.BasicAuth.Username != "" || c.BasicAuth.Password != "" {
+		auth = &authn.Basic{
+			Username: c.BasicAuth.Username,
+			Password: c.BasicAuth.Password,
 		}
-		return body, nil
-	default:
-		return nil, fmt.Errorf("unexpected status code %d: %s", resp.StatusCode, resp.Status)
 	}
+	opts := []remote.Option{remote.WithAuth(auth)}
+	if c.Insecure {
+		opts = append(opts, remote.WithTransport(insecureTransport()))
+	}
+	return opts
 }
 
-// todo: refactor this function, add opts for tls
-func (c *Registry) pushManifest(image string, manifest []byte) error {
-	ref, err := name.ParseReference(image)
-	if err != nil {
-		return fmt.Errorf("failed to parse image: %w", err)
+func (c *Registry) nameOptions() []name.Option {
+	if c.Insecure {
+		return []name.Option{name.Insecure}
 	}
-	url := fmt.Sprintf(
-		"http://%s/v2/%s/manifests/%s",
-		ref.Context().RegistryStr(),
-		ref.Context().RepositoryStr(),
-		ref.Identifier(),
-	)
-
-	req, err := http.NewRequest(http.MethodPut, url, bytes.NewBuffer(manifest))
-	if err != nil {
-		return fmt.Errorf("failed to create PUT request: %w", err)
-	}
-
-	req.SetBasicAuth(c.BasicAuth.Username, c.BasicAuth.Password)
-	req.Header.Set("Content-Type", "application/vnd.docker.distribution.manifest.v2+json")
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return fmt.Errorf("failed to execute request: %w", err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusCreated {
-		return fmt.Errorf("unexpected status code %d: %s", resp.StatusCode, resp.Status)
-	}
-
 	return nil
+}
+
+func insecureTransport() http.RoundTripper {
+	if transport, ok := remote.DefaultTransport.(*http.Transport); ok {
+		clone := transport.Clone()
+		if clone.TLSClientConfig == nil {
+			clone.TLSClientConfig = &tls.Config{}
+		} else {
+			clone.TLSClientConfig = clone.TLSClientConfig.Clone()
+		}
+		clone.TLSClientConfig.InsecureSkipVerify = true
+		return clone
+	}
+	return remote.DefaultTransport
+}
+
+func isManifestNotFound(err error) bool {
+	var transportErr *transport.Error
+	if errors.As(err, &transportErr) {
+		return transportErr.StatusCode == http.StatusNotFound
+	}
+	return false
 }
